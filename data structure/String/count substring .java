e65e9f55d746af4610572950664349b73341125b
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-29 01:15:34 -0700

    Create count substring .java

---
+++ data structure/String/count substring .java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+      
+      String binary="ravi";
+      int l = binary.length();
+      System.out.print(l*(l+1)/2);
+    }
+}
