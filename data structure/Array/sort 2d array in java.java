58f3ba7ad7c27487cb4bf6ff14b8f9168d9b10ae
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-30 06:14:36 -0700

    Update sort 2d array in java.java

--- data structure/Array/sort 2d array in java.java
+++ data structure/Array/sort 2d array in java.java
@@ -1,3 +1,4 @@
+//********first method******//
 import java.util.*;
 public class HelloWorld{
 
@@ -18,3 +19,30 @@ Arrays.sort(arr, new Comparator<int[]>(){
      }
      }
 }
+////*********************//
+
+///*****second method**//
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+         int arr[][]= {{2,2},{1,1},{3,3},{1,1}};
+class Mycom implements Comparator<int[]>
+{
+     public int compare(int[] a, int[] b){
+        return a[0] - b[0];
+}
+}
+Arrays.sort(arr, new Mycom());
+
+ for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++){
+            
+              System.out.print(arr[i][j]+ " ");     
+            }
+             System.out.println("");
+            
+     }
+     }
+}
+//**********//
