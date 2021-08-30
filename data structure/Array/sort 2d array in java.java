88da8ae4fdce0eecfd01a8ed6600b0dbe9057f8d
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-08-30 05:34:14 -0700

    Create sort 2d array in java.java

---
+++ data structure/Array/sort 2d array in java.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+         int arr[][]= {{2,2},{1,1},{3,3},{1,1}};
+Arrays.sort(arr, new Comparator<int[]>(){
+    public int compare(int[] a, int[] b){
+        return a[0] - b[0];
+    }
+});
+ for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++){
+            
+              System.out.print(arr[i][j]+ " ");     
+            }
+             System.out.println("");
+            
+     }
+     }
+}
