c07e32b78a2418abaa90b4e6f6eb1487075c07ec
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-09-02 04:49:54 -0700

    Update graph using hashmap .java

--- data structure/Graph/graph using hashmap .java
+++ data structure/Graph/graph using hashmap .java
@@ -1,12 +1,55 @@
 import java.util.*;
+class Path
+{
+    String dest;
+    int weight;
+    Path(String dest,int weight)
+    {
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
 public class HelloWorld{
 
+     HashMap<String,ArrayList<Path>> arr = new HashMap<>();
+     private  void as()
+     {
+         for(int i=0;i<4;i++)
+            arr.add(new ArrayList());
+     }
+     private void create_graph(String cur , String dest , int w)
+     {
+         Path edge = new Path(dest,w);
+         Path edge1 = new Path(cur,w);
+         arr.get(cur).add(edge);
+         arr.get(dest).add(edge1);
+         
+     }
+      private void print()
+      {
+          
+          for(int i=0;i<4;i++)
+           {
+               System.out.println("current  :"+i + " :");
+               for(Path p : arr.get(i))
+                 System.out.println("dest :"+p.dest +" weight : "+p.weight + " ");
+                 System.out.println("");
+           }
+           
+      }
+      
+     
+     
      public static void main(String []args){
          
-         HashMap<String ,List<String>> hm = new HashMap<>();
-         hm.put("indore" , Arrays.asList("delhi" , "mumbai"));
-         hm.put("mumbai" , Arrays.asList("delhi" , "indore"));
-         hm.put("delhi" , Arrays.asList("indore" , "mumbai"));
-         System.out.println("Hello World \n"+ hm);
+          
+          HelloWorld obj = new  HelloWorld();
+          obj.as();
+          obj.create_graph("indore","delhi",200);
+          obj.create_graph("indore","mumbai",100);
+          obj.create_graph("mumbai","bhopal",500);
+          obj.create_graph("bhopal","delhi",650);
+          obj.print();
+        //  System.out.println("enter number of edge");
      }
 }
