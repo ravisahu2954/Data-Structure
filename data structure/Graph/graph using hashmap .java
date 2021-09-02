04c86ddb3607c14e5c6298eb64323f7e5381c470
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-09-02 06:51:23 -0700

    Update graph using hashmap .java

--- data structure/Graph/graph using hashmap .java
+++ data structure/Graph/graph using hashmap .java
@@ -15,41 +15,42 @@ public class HelloWorld{
      private  void as()
      {
          for(int i=0;i<4;i++)
-            arr.add(new ArrayList());
+            arr.put("",new ArrayList());
      }
      private void create_graph(String cur , String dest , int w)
      {
          Path edge = new Path(dest,w);
          Path edge1 = new Path(cur,w);
+         if(!arr.containsKey(cur))
+          arr.put(cur ,new ArrayList());
+         if(!arr.containsKey(dest))
+          arr.put(dest ,new ArrayList());
          arr.get(cur).add(edge);
          arr.get(dest).add(edge1);
          
      }
       private void print()
       {
-          
-          for(int i=0;i<4;i++)
-           {
-               System.out.println("current  :"+i + " :");
-               for(Path p : arr.get(i))
-                 System.out.println("dest :"+p.dest +" weight : "+p.weight + " ");
-                 System.out.println("");
-           }
+          for (Map.Entry i : arr.entrySet()) {   
+            String key = (String)i.getKey();
+            ArrayList<Path> value = (ArrayList<Path>)i.getValue();
+    
+          for(Path val : value){
+          System.out.print(key +" -> " );
+          System.out.print(val.dest + " " + val.weight + " , ");
+          }
+          System.out.println("");
+           
+       }
            
       }
-      
-     
-     
-     public static void main(String []args){
+       public static void main(String []args){
          
-          
-          HelloWorld obj = new  HelloWorld();
-          obj.as();
+           HelloWorld obj = new  HelloWorld();
           obj.create_graph("indore","delhi",200);
           obj.create_graph("indore","mumbai",100);
           obj.create_graph("mumbai","bhopal",500);
           obj.create_graph("bhopal","delhi",650);
           obj.print();
-        //  System.out.println("enter number of edge");
      }
 }
