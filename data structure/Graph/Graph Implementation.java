f50cc1c2d76dba48bf50ec7eed1055633a90c247
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-09-02 23:09:16 -0700

    Update Graph Implementation.java

--- data structure/Graph/Graph Implementation.java
+++ data structure/Graph/Graph Implementation.java
@@ -9,15 +9,15 @@ class Path
         this.weight = weight;
     }
 }
-public class HelloWorld{
-
-     ArrayList<ArrayList<Path>> arr = new ArrayList<>();
-     private  void as()
+class Graph{
+    
+     private ArrayList<ArrayList<Path>> arr = new ArrayList<>();
+     public  void as()
      {
          for(int i=0;i<4;i++)
             arr.add(new ArrayList());
      }
-     private void create_graph(int cur , int dest , int w)
+     public void create_graph(int cur , int dest , int w)
      {
          Path edge = new Path(dest,w);
          Path edge1 = new Path(cur,w);
@@ -25,31 +25,27 @@ public class HelloWorld{
          arr.get(dest).add(edge1);
          
      }
-      private void print()
+     public void print_graph()
       {
-          
-          for(int i=0;i<4;i++)
-           {
-               System.out.println("current  :"+i + " :");
+        for(int i=0;i<4;i++)
+          {
+             System.out.println("current  :"+i + " :");
                for(Path p : arr.get(i))
                  System.out.println("dest :"+p.dest +" weight : "+p.weight + " ");
                  System.out.println("");
            }
-           
       }
-      
-     
-     
-     public static void main(String []args){
+ }
+public class HelloWorld{
+
+       public static void main(String []args){
          
-          
-          HelloWorld obj = new  HelloWorld();
+          Graph obj = new  Graph();
           obj.as();
           obj.create_graph(0,3,20);
           obj.create_graph(0,1,10);
           obj.create_graph(1,2,5);
           obj.create_graph(2,3,6);
-          obj.print();
-        //  System.out.println("enter number of edge");
+          obj.print_graph();
      }
 }
