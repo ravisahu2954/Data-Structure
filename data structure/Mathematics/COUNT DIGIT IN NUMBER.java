7ffb418ab02c6c89248ad28ef3eeac25b7719f30
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-10-28 20:17:39 +0530

    Create COUNT DIGIT IN NUMBER.java

---
+++ data structure/Mathematics/COUNT DIGIT IN NUMBER.java
@@ -0,0 +1,40 @@
+//RECURSION VERSION
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+  
+  static int count=0;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	      Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(f(n));
+  }
+	private static int f(int n)
+	{
+	    
+	    if(n==0)
+	      return count;
+	    count++;  
+	    return f(n/10);  
+	      
+	}
+}
+///////
+//ITERATIVE VERSION
+private static int f(int n)
+	{
+	   int count=0;
+	   while(n>0)
+	   {
+	         n=n/10;
+	         count++;
+	   }
+	    return count; 
+	 }
