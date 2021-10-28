17cee02a968b5410945455b2f457f39899ddf4d0
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-10-28 20:18:12 +0530

    Update COUNT DIGIT IN NUMBER.java

--- data structure/Mathematics/COUNT DIGIT IN NUMBER.java
+++ data structure/Mathematics/COUNT DIGIT IN NUMBER.java
@@ -5,14 +5,14 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+
+class Main
 {
   
   static int count=0;
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	      Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
         System.out.println(f(n));
   }
