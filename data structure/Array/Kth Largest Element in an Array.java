2f775b33ba2e2947161ecdce73d8882f39580da7
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2021-11-15 11:58:03 +0530

    Create Kth Largest Element in an Array.java

---
+++ data structure/Array/Kth Largest Element in an Array.java
@@ -0,0 +1,39 @@
+QUESTION LINK :https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+
+SOLUTION 1:
+
+import java.util.*;
+import java.io.*;
+public class Test1
+{
+    
+    public static void main(String []args)
+     {
+         
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++)
+         arr[i] = sc.nextInt();
+        
+        PriorityQueue<Integer> pq = new  PriorityQueue<>();
+        for(int i=0;i<n;i++)
+        {
+           if(pq.size()<k)
+              pq.add(arr[i]);
+           else
+            {
+                 
+                 if(pq.peek()<arr[i])
+                 {
+                    pq.poll();
+                    pq.add(arr[i]);
+                 }
+            }  
+        } 
+        System.out.println(pq.peek());       
+      }
+}
+
