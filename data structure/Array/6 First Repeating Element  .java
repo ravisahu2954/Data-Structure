bea3be1e0064ccbce79c6a67c0a9634744a1b29e
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2022-01-08 22:27:20 +0530

    Update 6 First Repeating Element  .java

--- data structure/Array/6 First Repeating Element  .java
+++ data structure/Array/6 First Repeating Element  .java
@@ -1,20 +1,16 @@
 link : https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1/
  using hashmap...........
 public static int firstRepeated(int[] arr, int n) {
-        
+       
         HashMap<Integer , Integer> hm = new HashMap<>();
         for(int i=0 ;i <arr.length ; i++)
         {
             if(hm.containsKey(arr[i]))
-               hm.put(arr[i],hm.get(arr[i])+1);
+               return i+1;
             else
             hm.put(arr[i],1);
         }
-        for(int i=0 ; i<arr.length ; i++)
-           {
-               if(hm.get(arr[i])>1)
-                 return i+1;
-           }
+       
         return -1;
         
     }
@@ -24,18 +20,15 @@ Expected Auxilliary Space: O(n)
 //*******//
  using HashSet ...........
          public static int firstRepeated(int[] arr, int n) {
-        int min=-1;
+        
         HashSet<Integer> hs = new HashSet<>();
         for(int i=n-1 ;i>=0 ; i--)
         {
             if(hs.contains(arr[i]))
-               min=i;
+               return i+1;
             else
             hs.add(arr[i]);
         }
-        if(min!=-1)
-        return min+1;
-        else
-         return min;
+        return -1;
         
     }
