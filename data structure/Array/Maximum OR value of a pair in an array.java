0d1ffe65a56754d9e76f6cc172f0d2f7e44374cc
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2022-04-27 15:10:27 +0530

    Update Maximum OR value of a pair in an array.java

--- data structure/Array/Maximum OR value of a pair in an array.java
+++ data structure/Array/Maximum OR value of a pair in an array.java
@@ -3,54 +3,33 @@ Output: 28
 (12, 16) is the pair with the maximum bitwise OR. 
 12 | 16 = 28
 
-SOLUTION 1 : maximum or value will be adjacent element in sorted array ;
- 
- import java.util.*;
-import java.io.*;
-public class Test1
+
+import java.util.Arrays;
+
+class Main{
+
+public static int maxOR(int[] arr, int n)
 {
-    
-    public static void main(String []args)
-     {
-         
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++)
-         arr[i] = sc.nextInt();
-        
-        Arrays.sort(arr); 
-        System.out.println(arr[n-1] | arr[n-2]);       
-      }
+	
+
+	int max_value = Arrays.stream(arr).max().getAsInt();
+	
+
+	int ans =0;
+	for(int i = 0; i < n; i++)
+	{
+		ans = Math.max(ans, (max_value | arr[i]));
+	}
+	return ans;
 }
-TIME COMPLEXITY : 0(n log(n))
 
-SOLUTION 2: find maximum and then traverse array and find another pair element who make or greatest;
-
-import java.util.*;
-import java.io.*;
-public class Test1
+public static void main(String[] args)
 {
-    
-    public static void main(String []args)
-     {
-         
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++)
-         arr[i] = sc.nextInt();
-        int max=0;
-        for(int i=0;i<n;i++)
-         {
-           if(arr[i]>max)
-              max=arr[i];
-         }
-         int ans=0;
-        for(int i=0;i<n;i++)
-          ans = Math.max(ans,arr[i] | max);
-        System.out.println(ans);       
-      }
+	int arr[] = { 3, 6, 8, 16 };
+	int n = arr.length;
+	
+	System.out.print(maxOR(arr, n));
+}
 }
 
 TIME COMPLEXITY : 0(n log(n))
