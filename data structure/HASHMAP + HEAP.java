503cff0f22a4173ff1dc9c34faa9947278ce0d21
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2022-08-02 00:31:33 +0530

    Update HASHMAP + HEAP.java

--- data structure/HASHMAP + HEAP.java
+++ data structure/HASHMAP + HEAP.java
@@ -48,23 +48,41 @@ class Solution {
 
 simple version
 ::
-	HashMap<Integer,Integer> hm= new HashMap<>();
-        int arr[]=new int [k];
-        for(int ele: nums){
-            hm.put(ele,hm.getOrDefault(ele,0)+1);
+	hashmap+head with integer :https://leetcode.com/submissions/detail/762595427/
+	
+	class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int ans[] = new int[k];
+        for(int i=0;i<nums.length;i++)
+            hm.merge(nums[i],1,Integer::sum);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(hm.get(b)-hm.get(a)));
+        int temp = 0;
+        for(int i : hm.keySet())
+        {
+            pq.add(i);
         }
-        
-        PriorityQueue <Integer> pq= new PriorityQueue<>((a,b) -> hm.get(a) - hm.get(b));
-        for(int n: hm.keySet()){
-            pq.add(n);
-            if(pq.size()>k){
-                pq.remove(); 
-            }
+        while(k-->0)
+            ans[temp++]=pq.poll();
+        return ans;
+    }
+}
+HashMap + headp with Character : https://leetcode.com/submissions/detail/762605776/
+
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++)
+            hm.merge(s.charAt(i),1,Integer::sum);
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)->(hm.get(b)-hm.get(a)));
+        for(char c:hm.keySet())
+            pq.add(c);
+        String ans="";
+        while(!pq.isEmpty())
+        {
+            char c = pq.poll(); 
+            ans+=String.valueOf(c).repeat(hm.get(c));
         }
-        
-        int i=0;
-        while(!pq.isEmpty()){
-            int num=pq.remove();
-            arr[i++]=num;
-        }
-        return arr;
+        return ans;
+    }
+}
