4693b8644b18be76d70c988da627b7a38afbf9a9
ravisahu2954 <82141425+ravisahu2954@users.noreply.github.com>
2022-08-02 00:18:42 +0530

    Update sort hashmap according key or value.java

--- data structure/sort hashmap according key or value.java
+++ data structure/sort hashmap according key or value.java
@@ -1,16 +1,18 @@
-
-import java.util.*;
-class HelloWorld {
-    public static void main(String[] args) {
+https://leetcode.com/problems/top-k-frequent-elements/
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> hm = new HashMap<>();
-    List<Integer> list = new ArrayList<>(Arrays.asList(1,1,2,4,4,5));
-      for(int i : list)
-         hm.merge(i,1,Integer::sum);
-        System.out.println(hm);
-    List<Map.Entry<Integer,Integer>> l = new ArrayList<>(hm.entrySet());
-    Collections.sort(l,(a,b)->(a.getKey()-a.getKey()));//sort by key but no need of this we can use treemap
-    Collections.sort(l,(a,b)->(a.getValue()-a.getValue()));//sort by value in increasing or decreasing order
-    for(Map.Entry<Integer,Integer> h : l)
-       System.out.println(h.getKey() + "  "+h.getValue());
+        int ans[] = new int[k];
+        for(int i=0;i<nums.length;i++)
+            hm.merge(nums[i],1,Integer::sum);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(hm.get(b)-hm.get(a)));
+        int temp = 0;
+        for(int i : hm.keySet())
+        {
+            pq.add(i);
+        }
+        while(k-->0)
+            ans[temp++]=pq.poll();
+        return ans;
     }
 }
